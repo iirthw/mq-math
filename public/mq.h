6baf1119c20af63811278fee0747ebee7e2c7230
iirthw <iirthw@gmail.com>
2021-12-25 18:31:28 +0200

    add discriminant function template

--- public/mq.h
+++ public/mq.h
@@ -16,4 +16,10 @@ namespace mq
     static inline bool isZero(float x) {
         return (x < internal::MQ_EPSILON) && (x > -internal::MQ_EPSILON);
     }
+
+    template <typename T>
+    float discriminant_squared(T a, T b, T c)
+    {
+        return b * b - 4 * a * c;
+    }
 } // namespace mq
