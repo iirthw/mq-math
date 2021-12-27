1a8e40ec80a1bb7f7712625aa1a798c0a8785f7e
iirthw <iirthw@gmail.com>
2021-12-27 22:03:49 +0200

    [mq] Refactor how PI constant is defined

--- public/mq.h
+++ public/mq.h
@@ -4,13 +4,16 @@
  * mq_math.h a.k.a. mq math
 */
 
+#define _USE_MATH_DEFINES
+#include <math.h> // Must be first include in the file.
+
 namespace mq
 {
     namespace internal
     {
         constexpr float MQ_EPSILON = 1.0e-7f;
-        constexpr float MQ_PI = 3.14159265359f;
-        constexpr float MQ_RADIANS_PER_DEGREE = MQ_PI / 180.f;
+        constexpr float PI = static_cast<float>(M_PI);
+        constexpr float MQ_RADIANS_PER_DEGREE = static_cast<float>(PI) / 180.f;
     }
     
     static inline bool isZero(float x) {
